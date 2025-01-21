f716e14c3e5edc5b4cf01c223082706a1264ba04
[Preet2006] <147617586+Preet2006@users.noreply.github.com>
2025-01-21 22:44:02 +0530

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::", // Enables IPv6 and IPv4 connections
-    port: 8080, // Sets the development server to run on port 8080
+    host: true, // Enables access via local network
+    port: 8080, // Server runs on port 8080
   },
-
   plugins: [
     react(),
-    mode === "development" && componentTagger(), // Enables the componentTagger plugin only in development mode
-  ].filter(Boolean), // Removes falsey values from the plugins array
-
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Creates an alias for the "src" directory
+      "@": path.resolve(__dirname, "src"), // Alias for the "src" directory
     },
   },
-
-  // Set base path conditionally for development and production
-  base: mode === "production" ? "/Todo/" : "/", // In development, use the root path '/'
+  base: mode === "production" ? "/repo-name/" : "/", // Update 'repo-name' with your GitHub repository name
 }));
